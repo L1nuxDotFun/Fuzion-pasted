22d4e0d15d939eb4480515b94b5e44c36f8fa2b4
Dylan Joseph Roberts <50863872+xDyln@users.noreply.github.com>
2019-06-12 19:55:43 +0100

    New Change logs

--- src/ATGUI/Windows/playerlist.cpp
+++ src/ATGUI/Windows/playerlist.cpp
@@ -24,13 +24,18 @@ void PlayerList::RenderWindow()
 
 	if (ImGui::Begin(XORSTR("About"), &PlayerList::showWindow, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_ShowBorders))
 	{
-		ImGui::Text("changelog release v2.1 WIP b8");
-		ImGui::Text("- removed mouse movements");
-		ImGui::Text("- removed player list");
-		ImGui::Text("- added rounded corners and fixed other gui issues");
-		ImGui::Text("- fixed the menu issues.");
-		ImGui::Text("- relocated fakelag to the hvh tab");
-		ImGui::Text("- relocated fakelag slider ammount to the hvh tab");
+		ImGui::Text("changelog release v2.1 WIP b1");
+		ImGui::Text("- 	removed mouse movements");
+		ImGui::Text("- 	removed player list");
+		ImGui::Text("- 	added rounded corners and fixed other gui issues");
+		ImGui::Text("- 	fixed the menu issues.");
+		ImGui::Text("- 	relocated fakelag to the hvh tab");
+		ImGui::Text("- 	relocated fakelag slider ammount to the hvh tab");
+		ImGui::Text("- 	fixed name changer");
+		ImGui::Text("- 	fixed auto shoot for some weapons");
+		ImGui::Text("- 	added chams types for weapons and fixed chams alpha on localplayer");
+		ImGui::Text("- 	fixed Arms color doesn't change on wireframe mode");
+		ImGui::Text("- 	added meme french bomb so now its le bom");
 		ImGui::Text("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
 		ImGui::Text("SPECIAL THANKS!");
 		ImGui::Text("Koutsie, Koutsie#5777 -Staying up till 5am helping me find a nice menu & color scheme.");
